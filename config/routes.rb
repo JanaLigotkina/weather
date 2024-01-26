d60beaf2803795d583ac2d82e2124d0f16f9690f
jana <janakolesnikova6293@gmail.com>
2024-01-26 14:51:03 +0400

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
     get :current
     get 'by_time', as: 'by_time_weather'
     get 'historical', :historical
-    get 'historical/max' => :historical_max
-    get 'historical/min' => :historical_min
-    get 'historical/avg' => :historical_avg
+    get 'historical/max', to: 'weather#historical_max'
+    get 'historical/min', to: 'weather#historical_min'
+    get 'historical/avg', to: 'weather#historical_avg'
   end
 
   get "health" => 'health#show'
